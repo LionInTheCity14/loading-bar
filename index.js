e8c94b19a47a1a80b5d2e06a13d60375aadce882
Varun Pal Singh <singhvarun928@gmail.com>
2023-04-21 16:08:41 +0530

    started the loading bar project

---
+++ index.js
@@ -0,0 +1,18 @@
+const containerEl = document.querySelector(".container");
+const counterEl = document.querySelector(".counter");
+const barEl = document.querySelector(".loading-bar-front");
+
+let idx = 0;
+
+// updateNum();
+
+function updateNum(){
+    counterEl.innerText = `${idx}%`;
+    barEl.style.width =  `${idx}%` 
+    idx++;
+    if(idx <= 100){
+        setTimeout(updateNum, 20);
+    }
+}
+
+barEl.addEventListener("mousemove", updateNum);
